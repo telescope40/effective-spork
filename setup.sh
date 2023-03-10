b8b6d637c1e82df848d2b16eae1eb18e2fcb6b43
louis.devictoria <telescope40@gmail.com>
2023-03-10 16:28:31 -0500

    update requirements

--- setup.sh
+++ setup.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
+echo "Make Setup.sh executable"
+
+chmod u+x setup.sh
+
+echo "Create Directory for Prometheus"
 
 mkdir prometheus
 
-mkdir pydir
+echo "apt update ubuntu"
 
 sudo apt-get update
+
+echo "Install Docker * Docker Compose"
 
 sudo apt-get install docker --yes --force-yes
 
 sudo apt-get install docker-compose --yes --force-yes
+
+apt install python-pip --yes --force-yes
+
+pip install jinja2
 
 apt install python3-pip --yes --force-yes
 
 pip3 install -r requirements
 
 pip install jinja2
-#python3 -m venv /pydir/
-
-mv docker-compose.yml monitor/.
 
 python3 getPublicIP.py
 
 python render.py
 
-chmod u+x setup.sh
+
