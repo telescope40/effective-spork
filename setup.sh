54b3bfa71da224130c255f9ce52f76ab5a2f4945
louis.devictoria <telescope40@gmail.com>
2023-03-10 14:52:22 -0500

    Update Python requirements

--- setup.sh
+++ setup.sh
@@ -9,6 +9,12 @@ sudo apt-get install docker --yes --force-yes
 
 sudo apt-get install docker-compose --yes --force-yes
 
+apt install python3-pip --yes --force-yes
+
+pip3 install -r requirements
+
+python3 -m venv /pydir/
+
 mv docker-compose.yml monitor/.
 
 python3 getPublicIP.py
